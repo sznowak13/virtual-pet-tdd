880f9560ca1a017aa56ef998ec40a66fe17f2dac
Szymon Nowak <sznowak13@gmail.com>
2019-02-26 15:27:08 +0100

    Created boilerpalte code for JavaFX

--- src/main/java/com/codecool/virtualPet/VirtualPet.java
+++ src/main/java/com/codecool/virtualPet/VirtualPet.java
@@ -1,4 +1,21 @@
 package com.codecool.virtualPet;
 
-public class VirtualPet {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class VirtualPet extends Application {
+    
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(new BorderPane(), 800, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Your amazing Pet");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
