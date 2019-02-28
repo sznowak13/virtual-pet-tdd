5b867dd321aa16ce12d7e19e3374c0fb0e4bf370
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 14:40:46 +0100

    Added more general setup resolution

--- src/main/java/com/codecool/virtualPet/View/DisplayConfig.java
+++ src/main/java/com/codecool/virtualPet/View/DisplayConfig.java
@@ -4,6 +4,8 @@ public class DisplayConfig {
 
     public static final double DISPLAY_WIDTH = 800;
     public static final double DISPLAY_HEIGHT = 600;
+    public static final double SETUP_WIDTH = DISPLAY_WIDTH / 2;
+    public static final double SETUP_HEIGHT = DISPLAY_HEIGHT / 2;
     private static final double PET_DISPLAY_RATIO = 0.75;
     static final double PET_DISPLAY_WIDTH = DISPLAY_WIDTH * PET_DISPLAY_RATIO;
 
