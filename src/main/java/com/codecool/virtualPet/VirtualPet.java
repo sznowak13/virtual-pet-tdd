2f2ee279118172d75f7b8fe79a9f597544647010
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 14:43:48 +0100

    Initializng mainController on app start

--- src/main/java/com/codecool/virtualPet/VirtualPet.java
+++ src/main/java/com/codecool/virtualPet/VirtualPet.java
@@ -1,23 +1,15 @@
 package com.codecool.virtualPet;
 
-import com.codecool.virtualPet.Controller.PetController;
-import com.codecool.virtualPet.View.DisplayConfig;
+import com.codecool.virtualPet.Controller.MainController;
+import com.codecool.virtualPet.View.SceneRouter;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class VirtualPet extends Application {
 
     public void start(Stage primaryStage) throws Exception {
-        PetController petController = new PetController();
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(petController.getPetView());
-        Scene scene = new Scene(borderPane, DisplayConfig.DISPLAY_WIDTH, DisplayConfig.DISPLAY_HEIGHT);
-
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Your amazing PetModel");
-        primaryStage.show();
+        MainController mainController = new MainController(new SceneRouter(primaryStage));
+        mainController.init();
     }
 
     public static void main(String[] args) {
