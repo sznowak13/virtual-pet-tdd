c061039c9ec3254e42cb0aebaa6b2018378796c8
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 14:41:03 +0100

    Added SceneRouter class
    Its purpose is to manage scene changing throughout
    the application lifetime this way we are separating
    this responsibility to one classyq

--- src/main/java/com/codecool/virtualPet/View/NonExistingSceneException.java
+++ src/main/java/com/codecool/virtualPet/View/NonExistingSceneException.java
@@ -1,4 +1,8 @@
 package com.codecool.virtualPet.View;
 
-public class NonExistingSceneException {
+public class NonExistingSceneException extends Exception {
+
+    public NonExistingSceneException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/codecool/virtualPet/View/SceneRouter.java
+++ src/main/java/com/codecool/virtualPet/View/SceneRouter.java
@@ -1,4 +1,39 @@
 package com.codecool.virtualPet.View;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SceneRouter {
+
+    private Stage window;
+    private Map<String, Scene> sceneMap = new HashMap<>();
+
+    public SceneRouter(Stage primaryStage) {
+        window = primaryStage;
+        window.show();
+    }
+
+    public void addScene(String sceneName, Scene scene) {
+        sceneMap.put(sceneName, scene);
+    }
+
+    public void changeSceneTo(String sceneName) throws NonExistingSceneException {
+        if (sceneMap.containsKey(sceneName)) {
+            window.setScene(sceneMap.get(sceneName));
+        } else {
+            throw new NonExistingSceneException("Cannot find a scene to go to: " + sceneName);
+        }
+    }
+
+    public void addSetupScene(PetSetupView view) {
+        addScene("SETUP", new Scene(view, DisplayConfig.SETUP_WIDTH, DisplayConfig.SETUP_HEIGHT));
+        try {
+            changeSceneTo("SETUP");
+        } catch (NonExistingSceneException e) {
+            e.printStackTrace();
+        }
+    }
 }
