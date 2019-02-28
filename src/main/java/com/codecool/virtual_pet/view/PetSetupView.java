42324b96c2849ed59e504accffed9bbfb119f297
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 16:42:09 +0100

    Switched from PropertyChangeSupport to custom EventSystem

--- src/main/java/com/codecool/virtual_pet/view/PetSetupView.java
+++ src/main/java/com/codecool/virtual_pet/view/PetSetupView.java
@@ -1,30 +1,43 @@
 package com.codecool.virtual_pet.view;
 
+import com.codecool.virtual_pet.event_system_lib.Event;
+import com.codecool.virtual_pet.event_system_lib.EventBroadcaster;
+import com.codecool.virtual_pet.event_system_lib.EventCode;
+import com.codecool.virtual_pet.event_system_lib.EventDispatcher;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-
-import java.beans.PropertyChangeSupport;
+import javafx.scene.text.Text;
 
 public class PetSetupView extends VBox {
 
     private TextField petNameField = new TextField();
     private Button createButton = new Button("Hatch!");
-    private PropertyChangeSupport support;
+    private Text errorMsg = new Text();
+    private EventBroadcaster eb = new EventBroadcaster();
 
     public PetSetupView() {
         super(15);
-        support = new PropertyChangeSupport(this);
         setAlignment(Pos.CENTER);
         petNameField.setPromptText("Enter name here...");
         petNameField.setAlignment(Pos.CENTER);
         petNameField.setMaxWidth(200);
+        createButton.setOnAction(event -> eb.broadcast(new Event(EventCode.CREATE_PET, petNameField.getCharacters())));
         getChildren().addAll(
                 new Label("Your pet name:"),
                 petNameField,
-                createButton
+                createButton,
+                errorMsg
         );
+    }
+
+    public void addEventDispatcher(EventDispatcher eventDispatcher) {
+        eb.addSubscriber(eventDispatcher);
+    }
+
+    public void displayError(String error) {
+        errorMsg.setText(error);
     }
 }
