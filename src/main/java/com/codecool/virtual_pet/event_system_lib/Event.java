6c13de28378904d220ef711e25f1ebc8826903ad
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 16:23:42 +0100

    Created custom event system library
    Event class is for creating events with eventCode
    and eventBody if neccessary

--- src/main/java/com/codecool/virtual_pet/event_system_lib/Event.java
+++ src/main/java/com/codecool/virtual_pet/event_system_lib/Event.java
@@ -1,4 +1,24 @@
 package com.codecool.virtual_pet.event_system_lib;
 
 public class Event {
+
+    private final EventCode eventCode;
+    private final Object eventData;
+
+    public Event(EventCode eventCode, Object eventData) {
+        this.eventCode = eventCode;
+        this.eventData = eventData;
+    }
+
+    public Event(EventCode eventCode) {
+        this(eventCode, null);
+    }
+
+    public EventCode getEventCode() {
+        return eventCode;
+    }
+
+    public Object getEventData() {
+        return eventData;
+    }
 }
