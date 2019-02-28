f9d0014c19cf96a4a87787c68ab56c095a469b49
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 14:39:22 +0100

    Added PetSetupController
    Dummy class with no functionalities

--- src/main/java/com/codecool/virtualPet/Controller/PetSetupController.java
+++ src/main/java/com/codecool/virtualPet/Controller/PetSetupController.java
@@ -1,4 +1,16 @@
 package com.codecool.virtualPet.Controller;
 
+import com.codecool.virtualPet.View.PetSetupView;
+
 public class PetSetupController {
+
+    private PetSetupView petSetupView;
+
+    public PetSetupController(PetSetupView petSetupView) {
+        this.petSetupView = petSetupView;
+    }
+
+    public PetSetupView getView() {
+        return petSetupView;
+    }
 }
