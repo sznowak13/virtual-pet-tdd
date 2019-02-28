88948ca2882e69a9c499b11b45b5ab7d74dafd2c
Szymon Nowak <sznowak13@gmail.com>
2019-02-28 14:43:28 +0100

    Added simple PetSetupView

--- src/main/java/com/codecool/virtualPet/View/PetSetupView.java
+++ src/main/java/com/codecool/virtualPet/View/PetSetupView.java
@@ -1,4 +1,30 @@
 package com.codecool.virtualPet.View;
 
-public class PetSetupView {
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.beans.PropertyChangeSupport;
+
+public class PetSetupView extends VBox {
+
+    private TextField petNameField = new TextField();
+    private Button createButton = new Button("Hatch!");
+    private PropertyChangeSupport support;
+
+    public PetSetupView() {
+        super(15);
+        support = new PropertyChangeSupport(this);
+        setAlignment(Pos.CENTER);
+        petNameField.setPromptText("Enter name here...");
+        petNameField.setAlignment(Pos.CENTER);
+        petNameField.setMaxWidth(200);
+        getChildren().addAll(
+                new Label("Your pet name:"),
+                petNameField,
+                createButton
+        );
+    }
 }
