00353116556cd0d2c25f4b8bc13295361a35fc0e
Szymon Nowak <sznowak13@gmail.com>
2019-03-10 16:35:54 +0100

    Added Config class

--- src/main/java/com/codecool/virtual_pet/model/Config.java
+++ src/main/java/com/codecool/virtual_pet/model/Config.java
@@ -1,5 +1,7 @@
 package com.codecool.virtual_pet.model;
 
-public class Config {
+public final class Config {
 
+    public static final double MAX_STAT_AMOUNT = 100;
+    public static final double MIN_STAT_AMOUNT = 0;
 }
