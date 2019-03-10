677642c457d8195de7da9acaaa9d5e0a1f2ec679
Szymon Nowak <sznowak13@gmail.com>
2019-03-10 16:36:42 +0100

    Added more modifiers to PetFood

--- src/main/java/com/codecool/virtual_pet/model/PetFood.java
+++ src/main/java/com/codecool/virtual_pet/model/PetFood.java
@@ -1,18 +1,32 @@
 package com.codecool.virtual_pet.model;
 
 public enum PetFood {
-    MEAT(30),
-    JELLY(15),
-    BREAD(20),
-    MILK(15);
+    MEAT(30, 20, 20),
+    JELLY(15, 15, 5),
+    BREAD(20, 10, 10),
+    MILK(15, 5, 15),
+    WATER(0, 5, -10),
+    FAT(40, -5, 30);
 
     private double hungerModifier;
+    private double happinessModifier;
+    private double tirednessModifier;
 
-    PetFood(double hungerModifier) {
+    PetFood(double hungerModifier, double happinessModifier, double tirednessModifier) {
         this.hungerModifier = hungerModifier;
+        this.happinessModifier = happinessModifier;
+        this.tirednessModifier = tirednessModifier;
     }
 
     public double getHungerModifier() {
         return hungerModifier;
+    }
+
+    public double getHappinessModifier() {
+        return happinessModifier;
+    }
+
+    public double getTirednessModifier() {
+        return tirednessModifier;
     }
 }
