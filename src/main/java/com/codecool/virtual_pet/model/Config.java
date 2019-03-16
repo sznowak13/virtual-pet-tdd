1c7709d77d876c1040d45e473dda341798b9e456
Szymon Nowak <sznowak13@gmail.com>
2019-03-16 19:45:21 +0100

    Lowered resting modifier

--- src/main/java/com/codecool/virtual_pet/model/Config.java
+++ src/main/java/com/codecool/virtual_pet/model/Config.java
@@ -4,6 +4,6 @@ public final class Config {
 
     public static final double MAX_STAT_AMOUNT = 100;
     public static final double MIN_STAT_AMOUNT = 0;
-    public static final double RESTING_MODIFIER = -5;
+    public static final double RESTING_MODIFIER = -2;
     public static final double FAVOR_MODIFIER = 10;
 }
